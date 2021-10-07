3058a03b459a0aa3920bf08d0ae0a1cd796f912f
Yui Fukada <fumufumu1510@gmail.com>
2021-10-07 15:45:34 +0900

    define sqrt

--- src/Cbrt.hs
+++ src/Cbrt.hs
@@ -1,9 +1,28 @@
 module Cbrt where
 
-type HontaiKakaku  = Double
-type ZeikomiKakaku = Double
-zeikomi :: HontaiKakaku -> ZeikomiKakaku
-zeikomi x = 1.1 * x
+import Prelude hiding (sqrt)
 
 cube :: Double -> Double
 cube x = x * x * x
+
+square :: Double -> Double
+square x = x * x
+
+{-
+sqrt :: Double -> Double
+もし、squt x = y だとしたら、　x = y * y
+-}
+
+sqrt :: Double -> Double
+sqrt x = sqrtIter 1.0 x
+
+sqrtIter :: Double -> (Double -> Double)
+sqrtIter guess x
+    | ok guess x = guess
+    | otherwise  = sqrtIter (improve guess x) x
+
+ok :: Double -> (Double -> Bool)
+ok guess x = abs (square guess - x) < 0.0001
+
+improve :: Double -> (Double -> Double)
+improve guess x = (guess + x / guess) / 2
